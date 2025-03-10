7328c9ded560e4e81199b4f8094dd4b744cb8970
28NS30 <neolava22@gmail.com>
2025-03-10 21:13:02 +0800

    Far From Done But Getting there

--- Neural Network/NN.cpp
+++ Neural Network/NN.cpp
@@ -4,8 +4,10 @@
 using namespace std;
 
 vector<vector<vector<double>>> weights = {{{0.2, 0.6}, {0.4, 0.1}}};
-vector<vector<double>> biases;
+vector<vector<double>> biases = {{0.7, 0.3}};
 int epochs = 1;
+double eta = 1e-10;
+
 double sigmoid(double x) {
     return 1.0 / (1.0 + exp(-x));
 }
@@ -23,13 +25,13 @@ vector<double> forwardPass(vector<double>input){
             newVector.push_back(cur);
         }
         input = newVector;
-        newVector = {};
+        newVector.clear();
     }
     return input;
 }
 
-void train(vector<vector<double>>inputs, vector<double> trueYs){
-    vector<vector<double>> dError; //derivative of the error with respect to the pre-actavtion weighted sum of a node
+void train(vector<vector<double>>inputs, vector<vector<double>> trueYs){
+    vector<vector<double>> dError(weights.size()); //derivative of the error with respect to the pre-actavtion weighted sum of a node
     // OUTPUT LAYER
     vector<vector<double>> predictions;
     for(auto tc: inputs){
@@ -41,24 +43,46 @@ void train(vector<vector<double>>inputs, vector<double> trueYs){
         for (int dataPoint = 0; dataPoint < predictions.size(); dataPoint++){
             // Stochastic GD -> updates every data point
             for (int layer = weights.size()-1; layer>=0; layer--){
-                // Calculate Gradient
-                // Gradient = Derivative of the Loss with respect to the preactivation output of the layer
-                for (int node = 0; node < weights[layer].size(); node++){
-                    for (int weight = 0; weight < weights[layer][weight].size(); weight++){
-                        // Update Weight
-                    }
-
-                    // Update Bias
+                double grad;
+                if (layer == weights.size()-1){
+                    // OUTPUT LAYER
+                    for (int node = 0; node < weights[layer].size(); node++){
+                        grad = (predictions[dataPoint][node] - trueYs[dataPoint][node])*(predictions[dataPoint][node] * (1 - predictions[dataPoint][node]));
+                        dError[weights.size() - 1 - layer].push_back(grad);
+                        for (int weight = 0; weight < weights[layer][node].size(); weight++){
+                                // Update Weight
+                                weights[layer][node][weight] -= eta*grad;
+                            }
+                            // Update Bias
+                            biases[layer][node] -= eta*grad;
+                    } 
                 }
+                else{
+                    // Calculate Gradient For Each node
+                    for (int curNode = 0; curNode<weights[layer].size(); curNode++){
+                        grad = 0;
+                        for (int nextNode = 0; nextNode < weights[layer+1].size(); nextNode++){
+                            grad += weights[layer+1][nextNode][curNode] * dError[layer+1][nextNode];
+                            }
+                    }
+                    
+                    
+                    // Gradient = Derivative of the Loss with respect to the preactivation output of the node
+                    for (int node = 0; node < weights[layer].size(); node++){
+                        for (int weight = 0; weight < weights[layer][node].size(); weight++){
+                            // Update Weight
+                            weights[layer][node][weight] -= eta*dError[layer][node];
+                        }
+                        // Update Bias
+                        biases[layer][node] -= eta*dError[layer][node];
+                    }}
             }
         }
     }
 }
 int main(){
-    // weights;
-    biases = {{0}, {0}};
     vector<double> test = forwardPass({0.5, 0.3});
-    for (int i; i < test.size(); i++){
+    for (int i=0; i < test.size(); i++){
         cout<<test[i] << " ";
     }
 
