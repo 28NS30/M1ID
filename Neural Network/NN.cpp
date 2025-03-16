f57aa379b62978dc1278e6b0d6f99084774721b8
28NS30 <neolava22@gmail.com>
2025-03-16 14:15:32 +0800

    Getting Kinda Close

--- Neural Network/NN.cpp
+++ Neural Network/NN.cpp
@@ -6,34 +6,36 @@ using namespace std;
 vector<vector<vector<double>>> weights = {{{0.2, 0.6}, {0.4, 0.1}}};
 vector<vector<double>> biases = {{0.7, 0.3}};
 int epochs = 1;
-double eta = 1e-10;
+double eta = 1e-9;
 
 double sigmoid(double x) {
     return 1.0 / (1.0 + exp(-x));
 }
-vector<double> forwardPass(vector<double>input){
-    vector<double> newVector;
+vector<vector<double>> forwardPass(vector<double>input){
+    vector<vector<double>>res;
+    res.push_back(input);
     double cur;
     for (int layer = 0; layer < weights.size(); layer ++){
+        vector<double> newVector;
         for (int node = 0; node < weights[layer].size(); node++){
             cur = 0;
-            for (int i = 0; i <input.size(); i++){
-                cur += input[i]*weights[layer][node][i];
+            for (int i = 0; i <res[layer].size(); i++){
+                cur += res[layer][i]*weights[layer][node][i];
             }
             cur += biases[layer][node];
             cur = sigmoid(cur);
             newVector.push_back(cur);
         }
-        input = newVector;
+        res.push_back(newVector);
         newVector.clear();
     }
-    return input;
+    return res;
 }
 
 void train(vector<vector<double>>inputs, vector<vector<double>> trueYs){
-    vector<vector<double>> dError(weights.size()); //derivative of the error with respect to the pre-actavtion weighted sum of a node
+    vector<vector<double>> dError(weights.size(), vector<double>()); //derivative of the error with respect to the pre-actavtion weighted sum of a node
     // OUTPUT LAYER
-    vector<vector<double>> predictions;
+    vector<vector<vector<double>>> predictions;
     for(auto tc: inputs){
         predictions.push_back(forwardPass(tc));
     }
@@ -47,11 +49,11 @@ void train(vector<vector<double>>inputs, vector<vector<double>> trueYs){
                 if (layer == weights.size()-1){
                     // OUTPUT LAYER
                     for (int node = 0; node < weights[layer].size(); node++){
-                        grad = (predictions[dataPoint][node] - trueYs[dataPoint][node])*(predictions[dataPoint][node] * (1 - predictions[dataPoint][node]));
-                        dError[weights.size() - 1 - layer].push_back(grad);
+                        grad = (predictions[dataPoint].back()[node] - trueYs[dataPoint][node])   *   (predictions[dataPoint].back()[node] * (1 - predictions[dataPoint].back()[node]));
+                        dError[layer].push_back(grad);
                         for (int weight = 0; weight < weights[layer][node].size(); weight++){
                                 // Update Weight
-                                weights[layer][node][weight] -= eta*grad;
+                                weights[layer][node][weight] -= predictions[dataPoint][layer][node]*eta*grad;
                             }
                             // Update Bias
                             biases[layer][node] -= eta*grad;
@@ -64,6 +66,7 @@ void train(vector<vector<double>>inputs, vector<vector<double>> trueYs){
                         for (int nextNode = 0; nextNode < weights[layer+1].size(); nextNode++){
                             grad += weights[layer+1][nextNode][curNode] * dError[layer+1][nextNode];
                             }
+                        dError[layer].push_back(grad);
                     }
                     
                     
@@ -71,7 +74,7 @@ void train(vector<vector<double>>inputs, vector<vector<double>> trueYs){
                     for (int node = 0; node < weights[layer].size(); node++){
                         for (int weight = 0; weight < weights[layer][node].size(); weight++){
                             // Update Weight
-                            weights[layer][node][weight] -= eta*dError[layer][node];
+                            weights[layer][node][weight] -= predictions[dataPoint][layer][node]*eta*dError[layer][node];
                         }
                         // Update Bias
                         biases[layer][node] -= eta*dError[layer][node];
@@ -81,9 +84,5 @@ void train(vector<vector<double>>inputs, vector<vector<double>> trueYs){
     }
 }
 int main(){
-    vector<double> test = forwardPass({0.5, 0.3});
-    for (int i=0; i < test.size(); i++){
-        cout<<test[i] << " ";
-    }
 
 }
