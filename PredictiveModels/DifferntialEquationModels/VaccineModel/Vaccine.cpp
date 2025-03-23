41b5a89ba31a30a9f5f4ab507a2200ac71bdd36e
28NS30 <neolava22@gmail.com>
2025-03-23 09:42:14 +0800

    SIR Model

--- PredictiveModels/DifferntialEquationModels/VaccineModel/Vaccine.cpp
+++ PredictiveModels/DifferntialEquationModels/VaccineModel/Vaccine.cpp
@@ -2,6 +2,29 @@
 #include <iostream>
 using namespace std;
 
+double B = 4.375e-11;
+double G = 0.14;
+double S = 8e9;
+double I = 1;
+double R = 0;
+double V = 0;
+double D = 0.0005;
+double dt = 1;
+vector<vector<double>> res;
+int cur = 0;
 int main(){
-    cout << "hello world";
+    while (I >= 1){
+        // res.push_back({round(S), round(I), round(R)});
+        cout << cur << "    " << "V: " << V << "    " << "S: " << round(S) << "    " << "I: " << round(I) << "    " << "R: " << round(R) << endl;
+        double new_V = V + S * D * dt;
+        double new_S = S - B * I * S * dt - S * D * dt;
+        double new_I = I + B * I * S * dt - G * I * dt;
+        double new_R = R + G * I * dt;
+        cur += 1;
+        V = new_V;
+        S = new_S;
+        I = new_I;
+        R = new_R;
+
+    }
 }
