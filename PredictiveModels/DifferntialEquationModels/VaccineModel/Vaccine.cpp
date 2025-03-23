ed97ada1fece152056e30b49de865bf0967e0a02
28NS30 <neolava22@gmail.com>
2025-03-23 09:45:51 +0800

    Add Vaccine Efficacy

--- PredictiveModels/DifferntialEquationModels/VaccineModel/Vaccine.cpp
+++ PredictiveModels/DifferntialEquationModels/VaccineModel/Vaccine.cpp
@@ -10,14 +10,15 @@ double R = 0;
 double V = 0;
 double D = 0.0005;
 double dt = 1;
+double efficacy = 0.9;
 vector<vector<double>> res;
 int cur = 0;
 int main(){
     while (I >= 1){
         // res.push_back({round(S), round(I), round(R)});
         cout << cur << "    " << "V: " << V << "    " << "S: " << round(S) << "    " << "I: " << round(I) << "    " << "R: " << round(R) << endl;
-        double new_V = V + S * D * dt;
-        double new_S = S - B * I * S * dt - S * D * dt;
+        double new_V = V + efficacy * S * D * dt;
+        double new_S = S - B * I * S * dt - efficacy * S * D * dt;
         double new_I = I + B * I * S * dt - G * I * dt;
         double new_R = R + G * I * dt;
         cur += 1;
