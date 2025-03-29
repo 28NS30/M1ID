ae256a63c67da9cb641cbca0f66dd8667540e2ae
28NS30 <neolava22@gmail.com>
2025-03-29 14:14:52 +0800

    Add read file

--- Regression/LinearRegression/FinalExamScores/ExamThroughStats.cpp
+++ Regression/LinearRegression/FinalExamScores/ExamThroughStats.cpp
@@ -1,7 +1,35 @@
 //data set from https://www.kaggle.com/datasets/himanshunakrani/student-study-hours?select=score_updated.csv
 #include <iostream>
+#include <ostream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
 using namespace std;
 
 int main(){
-    
+    fstream fout;
+    fout.open("data.csv", ios::out);
+    fstream fin;
+    fin.open("train.csv",ios::in);
+    //data collection
+    string row,temp,line,col;
+    vector<vector<string>> data;
+    vector<string> curr;
+while (getline(fin,line,'\n')) {
+    stringstream s(line);
+        // read every column and store it into col
+        while (getline(s,col,','))
+        {
+            // add all the column data into a vector
+            if (!col.empty()) {
+            curr.push_back(col);
+            } else {
+curr.push_back("0");
+            }
+        }
+            data.push_back(curr);
+            //pushes the vector into a 2d array data
+            curr.clear();
+        }
 }
